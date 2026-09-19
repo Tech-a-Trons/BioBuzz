4ae6312883bde82f3ef1ddf6262833cd8c62f9db
senthilnathan <pranavleaps@gmail.com>
2026-09-19 16:45:01 -0700

    Added comments and started auto base

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Season/Subsystems/BaseSubsystemExamples/Simplemotormech.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Season/Subsystems/BaseSubsystemExamples/Simplemotormech.java
@@ -6,7 +6,11 @@ import dev.nextftc.hardware.actuators.NextMotor;
 import dev.nextftc.robot.Mechanism;
 
 public class Simplemotormech implements Mechanism {
+
+    //Init the motor
     NextMotor motor = new NextMotor("motorname");
+
+    //Defines every single action that is needed
 
     public Command run() { return infinite(() -> motor.setThrottle(0.75)); }
     public Command burst() {return infinite(() -> motor.setThrottle(1));}
