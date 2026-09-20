7cc6881d50c16b0eae85ad4997a56dc112d3555f
senthilnathan <pranavleaps@gmail.com>
2026-09-19 17:31:39 -0700

    base auton finished

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Season/Auto/Simpleautobase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Season/Auto/Simpleautobase.java
@@ -1,5 +1,19 @@
 package org.firstinspires.ftc.teamcode.Season.Auto;
 
+import com.pedropathing.api.PoseFactory;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.ivy.Command;
+import com.pedropathing.ivy.Scheduler;
+import com.pedropathing.math.Pose;
+import com.pedropathing.paths.Path;
+
+import static com.pedropathing.api.Paths.curve;
+import static com.pedropathing.api.Paths.line;
+import static com.pedropathing.ivy.Scheduler.schedule;
+import static com.pedropathing.ivy.groups.Groups.sequential;
+import static com.pedropathing.ivy.pedro.PedroCommands.follow;
+
+import org.firstinspires.ftc.teamcode.Season.Auto.pedro.Constants;
 import org.firstinspires.ftc.teamcode.Season.Subsystems.BaseSubsystemExamples.Simplenextrobot;
 
 import dev.nextftc.robot.Telemetry;
@@ -11,22 +25,80 @@ public class Simpleautobase extends NextOpMode {
     Simplenextrobot robot;
     public Simpleautobase(Simplenextrobot robot) { super(robot); }
 
+    Follower follower;
+
+    private final PoseFactory poseFactory = PoseFactory.degrees();
+
+    private final Pose start = poseFactory.of(56, 8, 90);
+    private final Pose path1Start = poseFactory.of(56, 8, 270);
+    private final Pose path1 = poseFactory.of(10.1564, 44.9306, 180);
+    private final Pose path1Control1 = poseFactory.of(41.7153, 46.4628, 0);
+    private final Pose path1Control2 = poseFactory.of(30.3857, 47.8247, 0);
+    private final Pose point2 = poseFactory.of(56.1985, 123.5295, 90);
+    private final Pose point2Control1 = poseFactory.of(22.7903, 104.5877, 0);
+    private final Pose point3 = poseFactory.of(44.871, 132.223, 90);
+    private final Pose point4 = poseFactory.of(54.1255, 26.087, 270);
+    private final Pose point4Control1 = poseFactory.of(9.4032, 45.2721, 0);
+
+    public Path path1() {
+        return curve(path1Start, path1Control1, path1Control2, path1).linear(path1Start, path1);
+    }
+
+    public Path path2() {
+        return curve(path1, point2Control1, point2).linear(path1, point2);
+    }
+
+    public Path path3() {
+        return line(point2, point3).constant(point3);
+    }
+
+    public Path path4() {
+        return curve(point3, point4Control1, point4).linear(point3, point4);
+    }
+
+    private Command autoRoutine() {
+        return sequential(
+                follow(follower, path1()),
+                follow(follower, path2()),
+                follow(follower, path3()),
+                follow(follower, path4())
+        );
+    }
+
     //Runs periodically in init
     @Override
     public void disabledPeriodic() {
         Telemetry.log("Status", "Init");
+
+        Scheduler.reset();
+
+        follower = Constants.create(hardwareMap);
+        follower.setPose(start);
+        follower.update();
     }
 
     //Runs once at start
     @Override
     public void start() {
         Telemetry.log("Status", "Start");
+
+        schedule(autoRoutine());
     }
 
     //Runs periodically through the game
     @Override
     public void periodic() {
         Telemetry.log("Status", "Running");
+
+        follower.update();
+        Scheduler.execute();
+        // add your other methods needed in the loop here
+
+        telemetry.addData("X", follower.pose().x());
+        telemetry.addData("Y", follower.pose().y());
+        telemetry.addData("Heading", Math.toDegrees(follower.pose().heading()));
+        telemetry.addData("Follower Mode", follower.mode());
+        telemetry.update();
     }
 
     //Runs once at stop
